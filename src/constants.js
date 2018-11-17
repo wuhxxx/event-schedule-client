c864b8e1d22324fb9a72050b632910fc9e5ef07b
wuhxxx <wuhxxx@gmail.com>
2018-11-17 15:17:54 -0800

    Add authentication header prefix

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,5 @@ export const TIME_UNIT = 30;
 export const DEFAULT_FROM = 9 * 2 * 30;
 // default timeline end time;
 export const DEFAULT_TO = 18 * 2 * 30;
+// Authentication header prefix
+export const AUTH_HEADER = "Bearer";
