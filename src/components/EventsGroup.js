31610cf5c73f2a2e857d3b5561d092f2771eb79b
wuhxxx <wuhxxx@gmail.com>
2018-11-19 21:59:23 -0800

    weekday mobile view

--- src/components/EventsGroup.js
+++ src/components/EventsGroup.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import Responsive from "react-responsive";
 import "../styles/EventsGroup.css";
+
+const Tablet = props => <Responsive {...props} minWidth={800} />;
+const Mobile = props => <Responsive {...props} maxWidth={799} />;
 
 export default class EventsGroup extends Component {
     // prop types
@@ -19,13 +23,24 @@ export default class EventsGroup extends Component {
                 <div className="top-info">
                     <span>{this.props.weekday}</span>
                 </div>
-                <ul style={{ height: this.props.eventsGroupUlHeight }}>
-                    {/* <li className="single-event">
+                <Tablet>
+                    <ul style={{ height: this.props.eventsGroupUlHeight }}>
+                        {/* <li className="single-event">
                         <a href="#0">
                             <em class="event-name">Abs Circuit</em>
                         </a>
                     </li> */}
-                </ul>
+                    </ul>
+                </Tablet>
+                <Mobile>
+                    <ul>
+                        {/* <li className="single-event">
+                        <a href="#0">
+                            <em class="event-name">Abs Circuit</em>
+                        </a>
+                    </li> */}
+                    </ul>
+                </Mobile>
             </li>
         );
     }
