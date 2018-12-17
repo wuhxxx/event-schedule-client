6f76b47303482d596e8a80d39f39f7331b503619
wuhxxx <wuhxxx@gmail.com>
2018-12-16 23:43:12 -0800

    demo events from template

--- src/constants/demoEvents.js
+++ src/constants/demoEvents.js
@@ -1,42 +1,185 @@
-const demoEvents = [
+const absCircuit = [
     {
         title: "Abs Circuit",
+        location: "Wheeler 150",
         description:
             "Abs Circuit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
-        // event's color, decimal value of hex color number
-        color: {
-            type: Number,
-            required: true
-        },
-        // event's start time
-        startAt: {
-            // store as a number of minutes,
-            // value range [0, 1440], 1440 = 24 hour * 60 minute
-            // startTime must be less than endTime
-            type: Number,
-            min: 0,
-            max: 1440,
-            required: true
-        },
-        // event's end time
-        endAt: {
-            // store as a number of minutes,
-            // value range [0, 1440], 1440 = 24 hour * 60 minute
-            // endTime must be larger than startTime
-            type: Number,
-            min: 0,
-            max: 1440,
-            required: true
-        },
-        // event's day of week
-        weekday: {
-            // range [0,4], Monday to Friday
-            type: Number,
-            min: 0,
-            max: 4,
-            required: true
-        }
+        color: 5734290,
+        startAt: 9 * 60 + 30,
+        endAt: 10 * 60 + 30,
+        weekday: 0
+    },
+    {
+        title: "Abs Circuit",
+        location: "Wheeler 150",
+        description:
+            "Abs Circuit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 5734290,
+        startAt: 12 * 60 + 30,
+        endAt: 14 * 60,
+        weekday: 4
+    },
+    {
+        title: "Abs Circuit",
+        location: "Wheeler 150",
+        description:
+            "Abs Circuit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 5734290,
+        startAt: 9 * 60 + 30,
+        endAt: 10 * 60 + 30,
+        weekday: 3
+    },
+    {
+        title: "Abs Circuit",
+        location: "Wheeler 150",
+        description:
+            "Abs Circuit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 5734290,
+        startAt: 15 * 60 + 30,
+        endAt: 16 * 60 + 30,
+        weekday: 3
+    },
+    {
+        title: "Abs Circuit",
+        location: "Wheeler 150",
+        description:
+            "Abs Circuit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 5734290,
+        startAt: 13 * 60 + 30,
+        endAt: 15 * 60,
+        weekday: 1
     }
 ];
 
-export default demoEvents;
+const rowingWorkout = [
+    {
+        title: "Rowing Workout",
+        location: "Recreational Sports Facility",
+        description:
+            "Rowing Workout. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 4469843,
+        startAt: 11 * 60,
+        endAt: 12 * 60 + 30,
+        weekday: 0
+    },
+    {
+        title: "Rowing Workout",
+        location: "Recreational Sports Facility",
+        description:
+            "Rowing Workout. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 4469843,
+        startAt: 10 * 60,
+        endAt: 11 * 60,
+        weekday: 1
+    },
+    {
+        title: "Rowing Workout",
+        location: "Recreational Sports Facility",
+        description:
+            "Rowing Workout. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 4469843,
+        startAt: 12 * 60,
+        endAt: 13 * 60 + 45,
+        weekday: 2
+    },
+    {
+        title: "Rowing Workout",
+        location: "Recreational Sports Facility",
+        description:
+            "Rowing Workout. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 4469843,
+        startAt: 17 * 60,
+        endAt: 18 * 60 + 30,
+        weekday: 3
+    },
+    {
+        title: "Rowing Workout",
+        location: "Recreational Sports Facility",
+        description:
+            "Rowing Workout. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 4469843,
+        startAt: 10 * 60,
+        endAt: 11 * 60,
+        weekday: 4
+    }
+];
+
+const yoga = [
+    {
+        title: "Yoga Level 1",
+        description:
+            "Yoga 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 10664370,
+        startAt: 14 * 60,
+        endAt: 15 * 60 + 15,
+        weekday: 0
+    },
+    {
+        title: "Yoga Level 1",
+        description:
+            "Yoga 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 10664370,
+        startAt: 15 * 60 + 45,
+        endAt: 16 * 60 + 45,
+        weekday: 1
+    },
+    {
+        title: "Yoga Level 1",
+        description:
+            "Yoga 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 10664370,
+        startAt: 10 * 60 + 45,
+        endAt: 11 * 60 + 45,
+        weekday: 2
+    },
+    {
+        title: "Yoga Level 1",
+        description:
+            "Yoga 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 10664370,
+        startAt: 13 * 60 + 45,
+        endAt: 15 * 60,
+        weekday: 2
+    },
+    {
+        title: "Yoga Level 1",
+        description:
+            "Yoga 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 10664370,
+        startAt: 15 * 60 + 45,
+        endAt: 16 * 60 + 45,
+        weekday: 4
+    }
+];
+
+const restorativeYoga = [
+    {
+        title: "Restorative Yoga",
+        description:
+            "Restorative Yoga. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 16167015,
+        startAt: 11 * 60 + 30,
+        endAt: 13 * 60,
+        weekday: 1
+    },
+    {
+        title: "Restorative Yoga",
+        description:
+            "Restorative Yoga. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 16167015,
+        startAt: 9 * 60,
+        endAt: 10 * 60 + 15,
+        weekday: 2
+    },
+    {
+        title: "Restorative Yoga",
+        description:
+            "Restorative Yoga. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, unde, nulla. Vel unde deleniti, distinctio inventore quis molestiae perferendis, eum quo harum dolorum reiciendis sunt dicta maiores similique! Officiis repellat iure odio debitis enim eius commodi quae deserunt quam assumenda, ab asperiores reiciendis minima maxime odit laborum, libero veniam non?",
+        color: 16167015,
+        startAt: 12 * 60,
+        endAt: 13 * 60 + 45,
+        weekday: 3
+    }
+];
+
+export default [...absCircuit, ...rowingWorkout, ...yoga, ...restorativeYoga];
