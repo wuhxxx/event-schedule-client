101e6e5c6e92e7ab7aa49cc9a68368c73a68359b
wuhxxx <wuhxxx@gmail.com>
2018-12-18 17:35:49 -0800

    event action types

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -2,3 +2,12 @@
 export const USER_SIGN_IN = "USER_SIGN_IN";
 export const USER_LOG_OUT = "USER_LOG_OUT";
 // event action types
+export const SET_DEFAULT_EVENTS = "SET_DEFAULT_EVENTS";
+export const LOADING_USER_EVENTS = "LOADING_USER_EVENTS";
+export const SET_USER_EVENTS = "SET_USER_EVENTS";
+export const WAITING_API_RESPONSE = "WAITING_API_RESPONSE";
+export const ADD_EVENT = "ADD_EVENT";
+export const UPDATE_EVENT = "UPDATE_EVENT";
+export const DELETE_EVENTS = "DELETE_EVENTS";
+export const SET_ERROR = "SET_ERROR";
+export const CONSUME_ERROR = "CONSUME_ERROR";
