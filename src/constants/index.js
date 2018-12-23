b674d0bba9d9833b3e12f3379d56f511eca4b8c0
wuhxxx <wuhxxx@gmail.com>
2018-12-22 19:05:31 -0800

    fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,9 +25,9 @@ export const LOGINMODAL_FORM_SIGNIN = "Signin";
 export const LOGINMODAL_FORM_RESET = "Reset";
 export const LOGINMODAL_FORM_SIGNUP = "Signup";
 // login/signup form input fields
-export const USERNAME = "USERNAME";
-export const EMAIL = "EMAIL";
-export const PASSWORD = "PASSWORD";
+export const USERNAME = "username";
+export const EMAIL = "email";
+export const PASSWORD = "password";
 // login/signup form input fields error
 export const USERNAME_ERROR = "USERNAME_ERROR";
 export const EMAIL_ERROR = "EMAIL_ERROR";
